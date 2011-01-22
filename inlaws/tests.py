2f8a96e7c05a7c137c53dd113516aeefd525d8d4
Trey Hunner <treyhunner@gmail.com>
2011-01-21 17:01:38 -0800

    Fixed syntax error in tests

--- inlaws/tests.py
+++ inlaws/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 
 
-class django-inlawsTest(TestCase):
+class django_inlawsTest(TestCase):
     """
     Tests for django-inlaws
     """
-    def test_django-inlaws(self):
+    def test_django_inlaws(self):
         pass
